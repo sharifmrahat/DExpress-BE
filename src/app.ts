50fca90cdb22837ceca1f7438f5369280045e377
sharifmrahat <sharifmrahat@gmail.com>
2023-10-17 22:16:33 +0600

    cors origin is updated

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000" || "https://lorry-lagbe-fe.vercel.app/",
+    origin: ["http://localhost:3000", "https://lorry-lagbe-fe.vercel.app"],
     credentials: true,
   })
 );
